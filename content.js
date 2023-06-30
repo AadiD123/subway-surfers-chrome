f56d6c2e8a4240bc7889c8d240bf8b8c5689252c
jag-prabhakaran <73809351+jag-prabhakaran@users.noreply.github.com>
2023-06-29 23:03:32 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,8 +1,11 @@
 // Check if the videoElement already exists in the page
 var videoElement = document.getElementById('subwaySurfersVideo');
 
-// If not, create it
-if (!videoElement) {
+// If the videoElement exists, remove it
+if (videoElement) {
+    videoElement.parentNode.removeChild(videoElement);
+} else {
+    // If not, create it
     try {
         videoElement = document.createElement('video');
         videoElement.id = 'subwaySurfersVideo';
@@ -17,6 +20,19 @@ if (!videoElement) {
         videoElement.muted = true;
         videoElement.src = chrome.runtime.getURL('SubwaySurfers.mp4');
         document.body.appendChild(videoElement);
+
+        // Create close button
+        var closeButton = document.createElement('button');
+        closeButton.style.position = 'fixed';
+        closeButton.style.right = '10px';
+        closeButton.style.top = '10px';
+        closeButton.style.zIndex = '9999999';
+        closeButton.textContent = 'X';
+        closeButton.onclick = function () {
+            videoElement.parentNode.removeChild(videoElement);
+            this.parentNode.removeChild(this);
+        };
+        document.body.appendChild(closeButton);
     } catch (error) {
         // Do nothing, or log a custom error message if needed
     }
